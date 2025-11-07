e7fc67a3e4f106fec8b08a9575d1c4902cf31352
JLP-DAM <2223_jan.lazaro@iticbcn.cat>
2025-11-07 16:07:28 +0100

    08-Hashes acabat

--- 08-Hashes/src/Hashes.java
+++ 08-Hashes/src/Hashes.java
@@ -1,6 +1,8 @@
 import java.security.MessageDigest;
 import java.util.HexFormat;
 
+import java.util.Arrays;
+
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
@@ -16,7 +18,7 @@ public class Hashes {
     }
 
     public String getPBKDF2AmbSalt(String pw, String salt) throws Exception {
-        PBEKeySpec spec = new PBEKeySpec(pw.toCharArray(), salt.getBytes(), 10000, salt.length());
+        PBEKeySpec spec = new PBEKeySpec(pw.toCharArray(), salt.getBytes(), 65536, 128);
 
         SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
 
@@ -26,29 +28,52 @@ public class Hashes {
     public String forcaBruta(String alg, String hash, String salt) throws Exception {
         char[] charset = "abcdefABCDEF123456789!".toCharArray();
 
-        StringBuilder stringBuilder = new StringBuilder();
+        int longitutCharset = charset.length;
 
         npass = 0;
 
-        for (int characterIndex = 0; characterIndex < 6; characterIndex++) {
-            for (int characterIndex2 = 0; characterIndex2 < 6; characterIndex2++) {
-                for (int index = 0; index < charset.length; index++) {
-                    if (stringBuilder.length() > characterIndex) {
+        for (int index = 1; index <= 6; index++) {
+            
+            int[] indexes = new int[index];
+            Arrays.fill(indexes, 0);
+
+            while (true) {
+                
+                StringBuilder stringBuilder = new StringBuilder(index);
+
+                for (int index2 = 0; index2 < index; index2++) {
+                    stringBuilder.append(charset[indexes[index2]]);
+                }
+
+                String candidat = stringBuilder.toString();
+
+                String stringHash = alg.equals("SHA-512") ? getSHA512AmbSalt(candidat, salt) : getPBKDF2AmbSalt(candidat, salt);
+
+                npass++;
+
+                if (hash.equals(stringHash)) {
+                    return candidat;
+                }
+
+                
+                int posicio = index - 1;
+
+                while (posicio >= 0) {
+
+                    if (indexes[posicio] + 1 < longitutCharset) {
+                        indexes[posicio]++;
                         break;
-                    }
 
-                    if (stringBuilder.length() <= characterIndex2) {
-                        stringBuilder.append(charset[index]);
                     } else {
-                        stringBuilder.setCharAt(characterIndex2, charset[index]);
+                        indexes[posicio] = 0;
+                        posicio--;
+
                     }
-
-                    String stringHash = alg.equals("SHA-512") ? getSHA512AmbSalt(stringBuilder.toString(), salt) : getPBKDF2AmbSalt(stringBuilder.toString(), salt);
-
-                    npass++;
-
-                    if (hash.equals(stringHash)) {return stringBuilder.toString();};
-                }                
+                }
+                
+                if (posicio < 0) {
+                    break;
+                }
             }
         }
 
